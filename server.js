d06390f545dae65c2847b0e6a5c2e787b4effd0c
Evie Hoang <evie.h0325@gmail.com>
2023-10-30 14:01:46 -0700

    Added routes to root and speicic api/database

--- server.js
+++ server.js
@@ -8,17 +8,41 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'movies_db'
-  },
-  console.log(`Connected to the books_db database.`)
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'movies_db'
+    },
+    (err) => {
+        if (err) {
+            console.error('Database connection error: ' + err);
+        } else {
+            console.log('Connected to the movies_db database.');
+        }
+    }
 );
 
+app.get('/', function(req,res){
+    res.send('hi!')
+});
 
-// render the list
-db.query('SELECT * FROM movies_db', function (err, results) {
-    console.log(results);
-  });
+app.get('/api/movies', (req, res) => {
+    db.query('SELECT movie_name FROM movies_db.movies', function (err, results) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred while fetching data from the database' });
+        } else {
+            console.log(results);
+            res.json(results);
+        }
+    });
+});
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error('Server startup error: ' + err);
+    } else {
+        console.log(`Server running on port ${PORT}`);
+    }
+});
